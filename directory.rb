d701fd99691e04a571adb6fc911a5e2abeece71d
Serena Hathi <hathi.serena@gmail.com>
2018-01-13 17:20:41 +0000

    Refactored menu, turned students array into an instance variable and amended previous exercises to reflect this change

--- directory.rb
+++ directory.rb
@@ -1,35 +1,42 @@
+@students = []
+@line_width = 50
+@cohort = [:January, :February, :March, :April, :May, :June, :July, :August, :September, :October, :November, :December, ""]
+
 def interactive_menu
-  students = []
   loop do
-    # 1. Print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    # 2. Read the input and save it into a variable
-    selection = gets.chomp
-    # 3. Do what the user has asked
-    case selection
-    when "1"
-      students = input_students
-    when "2"
-      print_header
-      print(students)
-      print_footer(students)
-    when "9"
-      exit # this wil cause the program to exit
-    else
-      puts "I don't know what you meant, try again"
-    end      
+    print_menu
+    process(gets.chomp)
   end
 end
 
-@line_width = 50
-@cohort = [:January, :February, :March, :April, :May, :June, :July, :August, :September, :October, :November, :December, ""]
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students 
+    when "9"
+      exit
+    else
+      puts "I don't know what you mean, try again"   
+  end
+end
+
+def print_header
+  puts "The students of Villains Academy".center(@line_width)
+  puts "--------------".center(@line_width)
+end
 
 def input_students
 puts "Please enter the names of the students"
 puts "To finish, just hit return twice"
-students = []
+@students = []
 name = gets.strip.capitalize
 while !name.empty? do
 puts "Please enter #{name}'s cohort"
@@ -52,22 +59,22 @@ cohort = gets.strip.capitalize.to_sym
   country_of_birth = gets.strip.capitalize
   puts "What is #{name}'s height?"
   height = gets.strip
-  students << {name: name, cohort: cohort, hobby: hobby, country_of_birth: country_of_birth, height: height}
-  puts students.count == 1 ? "Now we have #{students.count} student" : "Now we have #{students.count} students"
+  @students << {name: name, cohort: cohort, hobby: hobby, country_of_birth: country_of_birth, height: height}
+  puts @students.count == 1 ? "Now we have #{@students.count} student" : "Now we have #{@students.count} students"
   puts "Please enter the student's name"
   name = gets.strip
 end
-students
 end
 
-def print_header
-  puts "The students of Villains Academy".center(@line_width)
-  puts "--------------".center(@line_width)
+def show_students
+  print_header
+  print_student_list
+  print_footer
 end
 
-def print(names)
-  if names.count >= 1
-    names.each do |student|
+def print_student_list
+  if @students.count >= 1
+    @students.each do |student|
       puts "Name: #{student[:name]}".center(@line_width)
       puts "Cohort: #{student[:cohort]}".center(@line_width)
       puts "Favourite hobby: #{student[:hobby]}".center(@line_width)
@@ -75,11 +82,11 @@ def print(names)
       puts "Height: #{student[:height]}".center(@line_width)
       puts " "
     end
-    end
+  end
 end
 
-def print_footer(names)
-  puts names.count == 1 ? "Overall, we have #{names.count} great student".center(@line_width) : "Overall, we have #{names.count} great students".center(@line_width)
+def print_footer
+  puts @students.count == 1 ? "Overall, we have #{@students.count} great student".center(@line_width) : "Overall, we have #{@students.count} great students".center(@line_width)
   puts " "
 end
 
@@ -87,49 +94,50 @@ interactive_menu
 
 
 
-=begin 
-Previous exercises
+=begin
+# Previous exercises - refactored to account for students array now being an instance variable
 
-Ex 1 - Print a number before the name of each student
-def print_with_index(names)
-  names.each_with_index do |student, index|
+# Ex 1 - Print a number before the name of each student
+def print_with_index
+  @students.each_with_index do |student, index|
     puts "#{index+1} #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-Ex 2 - Only print student names that begin with a certain letter
-def print_specific_letter(names, letter)
-  names.select do |student|
+# Ex 2 - Only print student names that begin with a certain letter
+def print_specific_letter(letter)
+  letter = letter.upcase
+  @students.select do |student|
     if student[:name].start_with?(letter)
       puts "#{student[:name]} (#{student[:cohort]} cohort)"
     end
     end
 end
 
-Ex 3 - Only print names that are less than 12 characters long
-def print_less_than_twelve_characters(names)
-  names.select do |student|
+# Ex 3 - Only print names that are less than 12 characters long
+def print_less_than_twelve_characters
+  @students.select do |student|
     if student[:name].length < 12
       puts "#{student[:name]} (#{student[:cohort]} cohort)"
     end
     end
 end
 
-Ex 4 - Use a loop rather than each() to iterate over the array of students
-def print_using_loop(names)
+# Ex 4 - Use a loop rather than each() to iterate over the array of students
+def print_using_loop
   counter = 0  
-  until counter == names.length do
-    puts "#{names[counter][:name]} #{names[counter][:cohort]} cohort"
+  until counter == @students.length do
+    puts "#{@students[counter][:name]} #{@students[counter][:cohort]} cohort"
     counter +=1
   end
 end
 
-Ex 8 - Print students by cohort
-def print_by_cohort(names)
+# Ex 8 - Print students by cohort
+def print_by_cohort
 
 sorted_by_cohort = {}  
 
-names.each do |student_hash|
+@students.each do |student_hash|
   student_cohort = student_hash[:cohort]
   student_names = student_hash[:name]
 
@@ -146,5 +154,15 @@ sorted_by_cohort.each do |key, value|
   puts value
 end
 end
-=end
 
+print_with_index
+puts "--"
+print_specific_letter("t")
+puts "--"
+print_less_than_twelve_characters
+puts "--"
+print_using_loop
+puts "--"
+print_by_cohort
+puts "--"
+=end
