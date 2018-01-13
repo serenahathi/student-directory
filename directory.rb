5cd3265486b14ba2d5b2126fb29b77969e1195d1
Serena Hathi <hathi.serena@gmail.com>
2018-01-13 11:21:27 +0000

    Added Ex 5 - input for hobbies, country of birth and height

--- directory.rb
+++ directory.rb
@@ -1,11 +1,33 @@
+# students = [
+#   {name: "Dr. Hannibal Lecter", cohort: :January, hobby: "Swimming", country_of_birth: "England", height: "5ft3"},
+#   {name: "Darth Vader", cohort: :January, hobby: "Cycling", country_of_birth: "France", height: "5ft4"},
+#   {name: "Nurse Ratched", cohort: :March, hobby: "Poetry", country_of_birth: "Italy", height: "5ft8"},
+#   {name: "Michael Corleone", cohort: :April, hobby: "Kickboxing", country_of_birth: "Germany", height: "5ft10"},
+#   {name: "Alex DeLarge", cohort: :May, hobby: "Yoga", country_of_birth: "Spain", height: "5ft3"},
+#   {name: "The Wicked Witch of the West", cohort: :June, hobby: "Netball", country_of_birth: "Greece", height: "5ft6"},
+#   {name: "Terminator", cohort: :June, hobby: "Football", country_of_birth: "Belgium", height: "6ft3"},
+#   {name: "Freddy Krueger", cohort: :August, hobby: "Squash", country_of_birth: "Norway", height: "6ft1"},
+#   {name: "The Joker", cohort: :August, hobby: "Tennis", country_of_birth: "Scotland", height: "5ft0"},
+#   {name: "Joffrey Baratheon", cohort: :October, hobby: "Painting", country_of_birth: "Switzerland", height: "6ft2"},
+#   {name: "Norman Bates", cohort: :October, hobby: "Fishing", country_of_birth: "England", height: "5ft8"}
+# ]
+
+# Ex 5 - Added input for hobbies, country of birth and height
 def input_students
 puts "Please enter the names of the students"
 puts "To finish, just hit return twice"
 students = []
-name = gets.chomp
+name = gets.chomp.capitalize
 while !name.empty? do
-  students << {name: name, cohort: :november}
+  puts "What is #{name}'s favourite hobby?"
+  hobby = gets.chomp.capitalize
+  puts "What is #{name}'s country of birth?"
+  country_of_birth = gets.chomp.capitalize
+  puts "What is #{name}'s height"
+  height = gets.chomp
+  students << {name: name, cohort: :November, hobby: hobby, country_of_birth: country_of_birth, height: height}
   puts "Now we have #{students.count} students"
+  puts "Please enter the student's name"
   name = gets.chomp
 end
 students
@@ -18,7 +40,12 @@ end
 
 def print(names)
   names.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    puts "Name: #{student[:name]}"
+    puts "Cohort: #{student[:cohort]}"
+    puts "Favourite hobby: #{student[:hobby]}"
+    puts "Country of birth: #{student[:country_of_birth]}"
+    puts "Height: #{student[:height]}"
+    puts " "
   end
 end
 
@@ -51,7 +78,7 @@ end
 def print_using_loop(names)
   counter = 0  
   until counter == names.length do
-    puts "#{names[counter][:name]} (#{names[counter][:cohort]} cohort)"
+    puts "#{names[counter][:name]} #{names[counter][:cohort]} cohort"
     counter +=1
   end
 end
@@ -60,11 +87,14 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
+# user input:
 students = input_students
+# use test data:
+# input_students 
 print_header
-# print(students)
+print(students)
 # print_with_index(students)
 # print_specific_letter(students,"S")
 # print_less_than_twelve_characters(students)
-print_using_loop(students)
+# print_using_loop(students)
 print_footer(students)
