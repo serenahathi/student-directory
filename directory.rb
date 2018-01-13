73144cc38d6f73c91b0d58319e60126635ea5485
Serena Hathi <hathi.serena@gmail.com>
2018-01-13 16:49:22 +0000

    Added basic interactive menu

--- directory.rb
+++ directory.rb
@@ -1,16 +1,27 @@
-# students = [
-#   {name: "Dr. Hannibal Lecter", cohort: :January, hobby: "Swimming", country_of_birth: "England", height: "5ft3"},
-#   {name: "Darth Vader", cohort: :January, hobby: "Cycling", country_of_birth: "France", height: "5ft4"},
-#   {name: "Nurse Ratched", cohort: :March, hobby: "Poetry", country_of_birth: "Italy", height: "5ft8"},
-#   {name: "Michael Corleone", cohort: :April, hobby: "Kickboxing", country_of_birth: "Germany", height: "5ft10"},
-#   {name: "Alex DeLarge", cohort: :May, hobby: "Yoga", country_of_birth: "Spain", height: "5ft3"},
-#   {name: "The Wicked Witch of the West", cohort: :June, hobby: "Netball", country_of_birth: "Greece", height: "5ft6"},
-#   {name: "Terminator", cohort: :June, hobby: "Football", country_of_birth: "Belgium", height: "6ft3"},
-#   {name: "Freddy Krueger", cohort: :August, hobby: "Squash", country_of_birth: "Norway", height: "6ft1"},
-#   {name: "The Joker", cohort: :August, hobby: "Tennis", country_of_birth: "Scotland", height: "5ft0"},
-#   {name: "Joffrey Baratheon", cohort: :October, hobby: "Painting", country_of_birth: "Switzerland", height: "6ft2"},
-#   {name: "Norman Bates", cohort: :October, hobby: "Fishing", country_of_birth: "England", height: "5ft8"}
-# ]
+def interactive_menu
+  students = []
+  loop do
+    # 1. Print the menu and ask the user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    # 2. Read the input and save it into a variable
+    selection = gets.chomp
+    # 3. Do what the user has asked
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit # this wil cause the program to exit
+    else
+      puts "I don't know what you meant, try again"
+    end      
+  end
+end
 
 # Ex 6 - Centered output
 @line_width = 50
@@ -46,7 +57,7 @@ cohort = gets.strip.capitalize.to_sym
   hobby = gets.strip.capitalize
   puts "What is #{name}'s country of birth?"
   country_of_birth = gets.strip.capitalize
-  puts "What is #{name}'s height"
+  puts "What is #{name}'s height?"
   height = gets.strip
   students << {name: name, cohort: cohort, hobby: hobby, country_of_birth: country_of_birth, height: height}
   # Ex 9 - Amended output to account for singular and plural
@@ -64,7 +75,7 @@ end
 
 def print(names)
 # Ex 12 - Do not print list if user has not inputted any students
-  if names.count > 1
+  if names.count >= 1
     names.each do |student|
       puts "Name: #{student[:name]}".center(@line_width)
       puts "Cohort: #{student[:cohort]}".center(@line_width)
@@ -73,17 +84,30 @@ def print(names)
       puts "Height: #{student[:height]}".center(@line_width)
       puts " "
     end
-  end
+    end
 end
 
-# Ex 1 - Print a number before the name of each student
+def print_footer(names)
+  # Ex 9 - Amended output to account for singular and plural
+  puts names.count == 1 ? "Overall, we have #{names.count} great student".center(@line_width) : "Overall, we have #{names.count} great students".center(@line_width)
+  puts " "
+end
+
+interactive_menu
+
+
+
+=begin 
+Previous exercises
+
+Ex 1 - Print a number before the name of each student
 def print_with_index(names)
   names.each_with_index do |student, index|
     puts "#{index+1} #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-# Ex 2 - Only print student names that begin with a certain letter
+Ex 2 - Only print student names that begin with a certain letter
 def print_specific_letter(names, letter)
   names.select do |student|
     if student[:name].start_with?(letter)
@@ -92,7 +116,7 @@ def print_specific_letter(names, letter)
     end
 end
 
-# Ex 3 - Only print names that are less than 12 characters long
+Ex 3 - Only print names that are less than 12 characters long
 def print_less_than_twelve_characters(names)
   names.select do |student|
     if student[:name].length < 12
@@ -101,7 +125,7 @@ def print_less_than_twelve_characters(names)
     end
 end
 
-# Ex 4 - Use a loop rather than each() to iterate over the array of students
+Ex 4 - Use a loop rather than each() to iterate over the array of students
 def print_using_loop(names)
   counter = 0  
   until counter == names.length do
@@ -110,7 +134,7 @@ def print_using_loop(names)
   end
 end
 
-# Ex 8 - Print students by cohort
+Ex 8 - Print students by cohort
 def print_by_cohort(names)
 
 sorted_by_cohort = {}  
@@ -132,23 +156,5 @@ sorted_by_cohort.each do |key, value|
   puts value
 end
 end
+=end
 
-def print_footer(names)
-  # Ex 9 - Amended output to account for singular and plural
-  puts names.count == 1 ? "Overall, we have #{names.count} great student".center(@line_width) : "Overall, we have #{names.count} great students".center(@line_width)
-  # puts "Overall, we have #{names.count} great students".center(@line_width)
-  puts " "
-end
-
-# user input:
-students = input_students
-# use test data:
-# input_students 
-print_header
-print(students)
-# print_with_index(students)
-# print_specific_letter(students,"S")
-# print_less_than_twelve_characters(students)
-# print_using_loop(students)
-print_footer(students)
-# print_by_cohort(students)
