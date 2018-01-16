86cc11eba77f8622660600bfbe9a9e59a14dfa7a
Serena Hathi <hathi.serena@gmail.com>
2018-01-16 10:35:40 +0000

    Removed extra comments

--- directory.rb
+++ directory.rb
@@ -122,21 +122,6 @@ end
 puts "Option 3 successfully completed".center(@line_width)
 end
 
-
-# def save_students
-#   puts "What file would you like to save to? Hit enter to save to students.csv"
-#   user_file_input = STDIN.gets.strip
-#   user_file_input.empty? ? user_file_input = "students.csv" : user_file_input
-#   file = File.open(user_file_input, "w") do |file|
-#   @students.each do |student|
-#     student_data = [student[:name], student[:cohort], student[:hobby], student[:country_of_birth], student[:height]]
-#     csv_line = student_data.join(",")
-#     file.puts csv_line
-#   end
-# end
-# puts "Option 3 successfully completed".center(@line_width)
-# end
-
 def select_where_to_load_from
     puts "What file would you like to load from? Hit enter to load students.csv"
   user_file_input = STDIN.gets.strip
@@ -162,16 +147,6 @@ def read_file(filename)
     puts "Option 4 successfully completed".center(@line_width)
     end
 
-# def read_file(filename)
-# file = File.open(filename, "r")
-#   file.readlines.each do |line|
-#   name, cohort, hobby, country_of_birth, height = line.chomp.split(",")
-#   add_student_to_array(name, cohort, hobby, country_of_birth, height)
-#   puts line
-# end
-# puts "Option 4 successfully completed".center(@line_width)
-# end
-
 def load_students_on_startup
   filename = ARGV.first # First argument from the command line 
   if filename.nil?
@@ -186,81 +161,3 @@ end
 
 load_students_on_startup
 interactive_menu
-
-
-
-
-
-
-=begin
-# Previous exercises - refactored to account for students array now being an instance variable
-
-# Ex 1 - Print a number before the name of each student
-def print_with_index
-  @students.each_with_index do |student, index|
-    puts "#{index+1} #{student[:name]} (#{student[:cohort]} cohort)"
-  end
-end
-
-# Ex 2 - Only print student names that begin with a certain letter
-def print_specific_letter(letter)
-  letter = letter.upcase
-  @students.select do |student|
-    if student[:name].start_with?(letter)
-      puts "#{student[:name]} (#{student[:cohort]} cohort)"
-    end
-    end
-end
-
-# Ex 3 - Only print names that are less than 12 characters long
-def print_less_than_twelve_characters
-  @students.select do |student|
-    if student[:name].length < 12
-      puts "#{student[:name]} (#{student[:cohort]} cohort)"
-    end
-    end
-end
-
-# Ex 4 - Use a loop rather than each() to iterate over the array of students
-def print_using_loop
-  counter = 0
-  until counter == @students.length do
-    puts "#{@students[counter][:name]} #{@students[counter][:cohort]} cohort"
-    counter +=1
-  end
-end
-
-# Ex 8 - Print students by cohort
-def print_by_cohort
-
-sorted_by_cohort = {}
-
-@students.each do |student_hash|
-  student_cohort = student_hash[:cohort]
-  student_names = student_hash[:name]
-
-if sorted_by_cohort[student_cohort] == nil
-  sorted_by_cohort[student_cohort] = []
-end
-  sorted_by_cohort[student_cohort] << student_names
-end
-
-puts "Students sorted by cohort:"
-sorted_by_cohort.each do |key, value|
-  puts " "
-  puts key
-  puts value
-end
-end
-
-print_with_index
-puts "--"
-print_specific_letter("t")
-puts "--"
-print_less_than_twelve_characters
-puts "--"
-print_using_loop
-puts "--"
-print_by_cohort
-puts "--"
-=end
